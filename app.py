47b27b3fd4b55a5412999367d3c288c0dd6bd6a5
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2025-01-17 12:14:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-import pandas as pd
 
 # Initialize connection to Google Sheets
 conn = st.connection('gsheets', type=GSheetsConnection)
 
-data = conn.read(worksheet='getquin', ttl='5')
-
-st.dataframe(data)
+try:
+    # Read data from the Google Sheet
+    data = conn.read(worksheet='getquin', ttl=5)
+    st.dataframe(data)
+except Exception as e:
+    st.error(f"An error occurred: {e}")
