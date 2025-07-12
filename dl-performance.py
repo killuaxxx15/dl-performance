6528119d3b74591aee5bb6779378b14e64ccf530
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2025-07-12 09:49:49 +0800

    Add files via upload

---
+++ dl-performance.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# Set Streamlit page configuration
+st.set_page_config(page_title='Portfolio', page_icon=':bar_chart:')
+
+# Display header for the dashboard
+st.header('Portfolio Performance')
+
+# Display the last update date
+st.markdown('##### Updated: 10/07/2025')
+
+portfolios = ["TAA_AlvaroSleeve", "TAA_1", "TAA_Aggregate"]
+
+selected_portfolio = st.selectbox('Select Portfolio', portfolios, index=0)
+
+st.subheader('Portfolio Performance Analysis')
+st.image(f'fig1_{selected_portfolio}.png', use_container_width=True)
+
+st.subheader('Portfolio Metrics & Analysis')
+st.image(f'fig2_{selected_portfolio}.png', use_container_width=True)
+st.image(f'fig3_{selected_portfolio}.png', use_container_width=True)
+
+st.subheader('Portfolio Exposure Analysis')
+st.image(f'fig4_{selected_portfolio}.png', use_container_width=True)
+
