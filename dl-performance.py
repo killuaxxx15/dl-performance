cb2ba7988a6e87a4ec534d98902b68b517193640
killuaxxx15 <153806499+killuaxxx15@users.noreply.github.com>
2025-12-03 11:17:05 +0800

    Update dl-performance.py

--- dl-performance.py
+++ dl-performance.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title='Portfolio', page_icon=':bar_chart:')
 st.header('Portfolio Performance')
 
 # Display the last update date
-st.markdown('##### Updated: 21/11/2025')
+st.markdown('##### Updated: 02/12/2025')
 
 portfolios = ["AlvaroSleeve", "TAA1"]
 
@@ -27,6 +27,7 @@ st.image(f'fig5_{selected_portfolio}.png', use_container_width=True)
 st.subheader('Individual Asset Analysis')
 st.image(f'fig6_{selected_portfolio}.png', use_container_width=True)
 st.image(f'fig7_{selected_portfolio}.png', use_container_width=True)
+
 
 
 
